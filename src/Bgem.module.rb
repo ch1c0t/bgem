59c954ac794e59844c9789ef8287943e69a2021e
Anatoly Chernow <chertoly@gmail.com>
2017-04-11 00:56:24 +0300

    Fix Bgem.run to make it work finally

--- src/Bgem.module.rb
+++ src/Bgem.module.rb
@@ -13,9 +13,9 @@ SOURCE_FILE = Dir['src/*.rb'][0]
 CONFIG_FILE = "#{Dir.pwd}/bgem/config.rb"
 
 class << self
-  def run config_file
-    config = Config.new (config_file or CONFIG_FILE)
-    target = TargetFile.new config.output, config.scope.reverse
+  def run config_file = CONFIG_FILE
+    config = Config.new config_file
+    target = TargetFile.new config.output, config.scope&.reverse
     target.write SourceFile.new(config.entry).to_s
     target
   end
