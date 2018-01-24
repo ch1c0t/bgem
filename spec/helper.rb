768ae483a14f2b63168879512135e0681b2fa109
Anatoly Chernow <chertoly@gmail.com>
2018-01-24 17:16:56 +0200

    Trying to isolate with chroot for mutation testing
    Leads to the LoadError inside the vm if not commented out
    
    /var/lib/gems/2.3.0/gems/rspec-core-3.6.0/lib/rspec/core/formatters.rb:206:in
    `require': cannot load such file -- rspec/core/formatters/
    progress_formatter (LoadError)
            from
            /var/lib/gems/2.3.0/gems/rspec-core-3.6.0/lib/rspec/core/formatters.rb:206:in
            `built_in_formatter'

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,2 +1,9 @@
 require 'hobby/devtools/rspec_helper'
 require 'bgem'
+
+=begin
+require 'fileutils'
+testing_directory = "/tmp/testing/bgem${$$}"
+FileUtils.mkdir_p testing_directory
+Dir.chroot testing_directory
+=end
