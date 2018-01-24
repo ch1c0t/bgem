cea00352826bd2e95299df2867ee5140030a9fe5
Anatoly Chernow <chertoly@gmail.com>
2018-01-24 16:08:10 +0200

    Patch String#indent instead of refining

--- src/Bgem.module.rb
+++ src/Bgem.module.rb
@@ -1,12 +1,10 @@
 require 'pathname'
 
-using Module.new {
-  refine String do
-    def indent number
-      lines.map { |line| "#{' '*number}#{line}" }.join
-    end
+class ::String
+  def indent number
+    lines.map { |line| "#{' '*number}#{line}" }.join
   end
-}
+end
 
 INDENT = 2
 SOURCE_FILE = Dir['src/*.rb'][0]
