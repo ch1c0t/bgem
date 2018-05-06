b4e3b8607b0274d6fc40702c1210b02fa4eabbcf
Anatoly Chernow <chertoly@gmail.com>
2018-05-07 02:16:53 +0300

    More specs for Write

---
+++ spec/unit/Write._spec.rb
@@ -0,0 +1,45 @@
+require 'helper'
+require 'tempfile'
+require 'ostruct'
+
+describe Bgem::Write do
+  let(:file) { Tempfile.new }
+
+  let :config do
+    OpenStruct.new output: file,
+      scope: ['class C', 'module M']
+  end
+
+  let(:write) { described_class.new config }
+
+  it 'writes newlines at the end' do
+    config = OpenStruct.new output: file
+    write = described_class.new config
+
+    write['smt']
+    expect(file.read).to eq "smt\n"
+  end
+
+  it 'writes to the nested scopes' do
+    write['smt']
+
+    source = <<~S
+      module M
+        class C
+          smt
+        end
+      end
+    S
+
+    expect(file.read).to eq source
+  end
+
+  it 'creates the directory for the file' do
+    file, dirname = Tempfile.new, double
+    file.define_singleton_method(:dirname) { dirname }
+    write.instance_variable_set :@file, file
+
+    expect(dirname).to receive :mkpath
+    write['smt']
+  end
+end
