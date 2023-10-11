995484dfdf55a424069dc63c6d47099b8b474a98
Anatoly Chernov <chertoly@gmail.com>
2023-10-11 04:19:52 +0300

    Remove spec timeout

--- spec/helper.rb
+++ spec/helper.rb
@@ -5,13 +5,3 @@ require 'ostruct'
 require 'rspec/power_assert'
 RSpec::PowerAssert.example_assertion_alias :assert
 RSpec::PowerAssert.example_group_assertion_alias :assert
-
-RSpec.configure do |config|
-  unless ENV['PRY']
-    require 'timeout'
-
-    config.around :each do |example|
-      Timeout.timeout 5, &example
-    end
-  end
-end
