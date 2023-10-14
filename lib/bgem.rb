51b43bebcaa3472ce867fa81ca66111fd434c020
Anatoly Chernov <chertoly@gmail.com>
2023-10-14 23:15:00 +0300

    self.hook StandardHooks

--- lib/bgem.rb
+++ lib/bgem.rb
@@ -51,25 +51,26 @@ module Bgem
           'erb',
         ]
         
-        def pre
-          patterns = EXTs.flat_map do |ext|
-            ["pre.#{@name}/*.#{ext}"].map do |pattern|
-              @dir.join pattern
-            end
-          end
+        def self.hook hook_name, default: false
+          define_method hook_name do
+            stubs = ["#{hook_name}.#{@name}/*."]
         
-          concatenate *patterns
+            if default
+              stubs << "#{@name}/*."
+            end
+        
+            patterns = EXTs.flat_map do |ext|
+              stubs.map do |string|
+                @dir.join (string + ext)
+              end
+            end
+        
+            concatenate *patterns
+          end
         end
         
-        def post
-          patterns = EXTs.flat_map do |ext|
-            ["#{@name}/*.#{ext}", "post.#{@name}/*.#{ext}"].map do |pattern|
-              @dir.join pattern
-            end
-          end
-        
-          concatenate *patterns
-        end
+        hook :post, default: true
+        hook :pre
         
         def concatenate *patterns
           Dir[*patterns].sort.map do |file|
