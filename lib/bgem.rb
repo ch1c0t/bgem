9ec973d8504d50ddb1b3f25c227495202759f81a
Anatoly Chernov <chertoly@gmail.com>
2023-10-16 03:17:05 +0300

    Extract #sort_with_order_file

--- lib/bgem.rb
+++ lib/bgem.rb
@@ -79,25 +79,29 @@ module Bgem
               directory.join "*.#{ext}"
             end
           
+            files = Dir[*patterns]
             order_file = directory.join 'order'
           
             if order_file.exist?
-              order = order_file.readlines.map &:chomp
-              files = Dir[*patterns]
-          
-              files_ordered_to_be_first = order.inject([]) do |array, name|
-                files_starting_with_name = files.select do |file|
-                  basename = File.basename file
-                  basename.start_with? "#{name}."
-                end
-                array += files_starting_with_name
-              end
-          
-              other_files = files - files_ordered_to_be_first
-              files_ordered_to_be_first += other_files.sort
+              sort_with_order_file order_file, files
             else
-              Dir[*patterns].sort
+              files.sort
             end
+          end
+          
+          def sort_with_order_file order_file, files
+            order = order_file.readlines.map &:chomp
+          
+            files_ordered_to_be_first = order.inject([]) do |array, name|
+              files_starting_with_name = files.select do |file|
+                basename = File.basename file
+                basename.start_with? "#{name}."
+              end
+              array += files_starting_with_name
+            end
+          
+            other_files = files - files_ordered_to_be_first
+            files_ordered_to_be_first += other_files.sort
           end
         end
       
