0982a93bc0b10d588e5875a81a868cffa13ad0d9
Anatoly Chernov <chertoly@gmail.com>
2023-10-16 03:05:55 +0300

    Move StandardHooks' helpers

--- src/Bgem/pre.Output/pre.Ext/StandardHooks.module.rb
+++ src/Bgem/pre.Output/pre.Ext/StandardHooks.module.rb
@@ -1,42 +1,5 @@
 extend DSL
+include Helpers
 
 hook :post, default: true
 hook :pre
-
-def concatenate files
-  files.map do |file|
-    Output.new(file, indent: INDENT).to_s
-  end.join "\n\n"
-end
-
-def file_extensions
-  constants = Bgem::Output::Ext.constants
-  constants.delete :StandardHooks
-  constants.map &:downcase
-end
-
-def sorted_files_in directory
-  patterns = file_extensions.map do |ext|
-    directory.join "*.#{ext}"
-  end
-
-  order_file = directory.join 'order'
-
-  if order_file.exist?
-    order = order_file.readlines.map &:chomp
-    files = Dir[*patterns]
-
-    files_ordered_to_be_first = order.inject([]) do |array, name|
-      files_starting_with_name = files.select do |file|
-        basename = File.basename file
-        basename.start_with? "#{name}."
-      end
-      array += files_starting_with_name
-    end
-
-    other_files = files - files_ordered_to_be_first
-    files_ordered_to_be_first += other_files.sort
-  else
-    Dir[*patterns].sort
-  end
-end
