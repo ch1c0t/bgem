62b87798ef2787542a0214d8c48fbae948881a0c
Anatoly Chernov <chertoly@gmail.com>
2025-08-23 22:18:57 +0300

    Release 0.2.2

--- lib/bgem.rb
+++ lib/bgem.rb
@@ -10,7 +10,7 @@ module Bgem
     end
   end
 
-  VERSION = '0.2.0'
+  VERSION = '0.2.2'
   
   require 'pathname'
   require 'string/indent'
--- src/Bgem.module.rb
+++ src/Bgem.module.rb
@@ -1,4 +1,4 @@
-VERSION = '0.2.0'
+VERSION = '0.2.2'
 
 require 'pathname'
 require 'string/indent'
